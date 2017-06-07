086b444c08ecf5587e47f6aa968e54003a80f131
chewie <linsh@oregonstate.edu>
2017-06-07 10:35:09 -0700

    clean up

--- projects/linsh/FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
+++ projects/linsh/FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
@@ -28,7 +28,7 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class UrlValidatorTest extends TestCase {
 
-	private boolean printStatus = true;
+	private boolean printStatus = false;
 	private boolean printIndex = false;//print index that indicates current scheme,host,port,path, query test were using.
 
 	public UrlValidatorTest(String testName) {
@@ -47,79 +47,75 @@ public class UrlValidatorTest extends TestCase {
 		UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
 		boolean expected, result;
 		String url;
-		
+
 		// Test the scheme
 		expected = true;
 		url = "ftp://go.cc/t123";
 		//System.out.println("Testing the URL scheme:");
 		result = urlVal.isValid(url);
 		assertEquals(url, expected, result);
-		
+
 		url = "http:/go.cc/t123";
 		expected = false;
 		result = urlVal.isValid(url);
 		assertEquals(url, expected, result);
-		
+
 	}
 
 	public void testYourSecondPartition(){
 		UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
 		boolean expected, result;
 		String url;
-		
+
 		// Test the authority
 		expected = true;
 		url = "i3://go.cc/helloworld";
 		//System.out.println("Testing the URL authority:");
 		result = urlVal.isValid(url);
 		assertEquals(url, expected, result);
-		
+
 		url = "i3://256.256.256.256/helloworld";
 		expected = false;
 		result = urlVal.isValid(url);
 		assertEquals(url, expected, result); // assertion fails
 	}
-	
+
 	public void testYourThirdPartition() {
 		UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
 		boolean expected, result;
 		String url;
-		
+
 		//Test the path
-		expected = true;
-		url = "ieee://xyz.xyz/path/to/somewhere";
-		result = urlVal.isValid(url);
-		assertEquals(url, expected, result);
-		
+
 		url = "ieee://xyz.xyz/..";
 		expected = false;
 		result = urlVal.isValid(url);
 		assertEquals(url, expected, result);
 	}
-	
+
 	public void testYourFourthPartition() {
 		UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
 		boolean expected, result;
 		String url;
-		
+
 		//Test the query
 		expected = true;
 		url = "http://google.com?key=value";
 		result = urlVal.isValid(url);
 		assertEquals(url, expected, result);
 	}
-	
+
 	public void testYourFifthPartition() {
 		UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
 		boolean expected, result;
 		String url;
-		
+
 		//Test the port
 		expected = true;
 		url = "http://google.com:65636";
 		result = urlVal.isValid(url);
 		assertEquals(url, expected, result);
-		
+
 		url = "http://google.com:-1";
 		expected = false;
 		result = urlVal.isValid(url);
@@ -132,8 +128,8 @@ public class UrlValidatorTest extends TestCase {
 		int partnum = 5; // # of url parts we want to test
 		int loopnum = 10; //10000
 
-		for(int i = 0; i<loopnum; i++) { 
-			StringBuffer testBuffer = new StringBuffer();  
+		for(int i = 0; i<loopnum; i++) {
+			StringBuffer testBuffer = new StringBuffer();
 			boolean expected = true;
 			int[] randomPick = randomIndex();
 
@@ -141,18 +137,17 @@ public class UrlValidatorTest extends TestCase {
 			// pick element for each url compoenents
 			for( int j = 0; j < partnum; j++){
 				component = (ResultPair) testUrlParts[j][randomPick[j]];
-				testBuffer.append(component.item);                               
-				expected &= component.valid;  
-				if (printStatus) {
+				testBuffer.append(component.item);
+				expected &= component.valid;
+				if (printStatus && printStatus) {
 					System.out.println(component.item + " -> " + component.valid);
 				}
 			}
-			String url = testBuffer.toString();                                   
-			boolean result = urlVal.isValid(url);  
-			if(result!=expected){                                                    
-				System.out.println(url);                                          
+			String url = testBuffer.toString();
+			boolean result = urlVal.isValid(url);
+			if(result!=expected){
 			}
-			assertEquals(url, expected, result);     
+			assertEquals(url, expected, result);
 		}
 	}
 
@@ -224,87 +219,9 @@ public class UrlValidatorTest extends TestCase {
 
 	/*
 	Object[][] testUrlParts = {testUrlScheme, testUrlAuthority, testUrlPort,
-		testPath,testUrlQuery}; 
+		testPath,testUrlQuery};
 		*/
 
 	Object[][] testUrlParts = {testUrlSchemeNew, testUrlAuthorityNew, testUrlPortNew,
-		testPathNew,testUrlQueryNew}; 
+		testPathNew,testUrlQueryNew};
 }
-/*
-	ResultPair[] testUrlScheme = {new ResultPair("http://", true),              
-		new ResultPair("ftp://", true),                                
-		new ResultPair("h3t://", true),                 
-		new ResultPair("3ht://", false),                
-		new ResultPair("http:/", false),                
-		new ResultPair("http:", false),                 
-		new ResultPair("http/", false),                 
-		new ResultPair("://", false),                   
-		new ResultPair("", true)};  
-
-
-	ResultPair[] testUrlPort = {new ResultPair(":80", true),                    
-		new ResultPair(":65535", true),                   
-		new ResultPair(":0", true),                       
-		new ResultPair("", true),                         
-		new ResultPair(":-1", false),                     
-		new ResultPair(":65636", true),                   
-		new ResultPair(":65a", false)                     
-	};                                           
-
-	ResultPair[] testUrlQuery = {new ResultPair("?action=view",true),                         
-		new ResultPair("?action=edit&mode=up", true),    
-		new ResultPair("", true)                         
-	}; 
-
-	ResultPair[] testUrlAuthority = {new ResultPair("www.google.com", true),    
-		new ResultPair("go.com", true),              
-		new ResultPair("go.au", true),               
-		new ResultPair("0.0.0.0", true),             
-		new ResultPair("255.255.255.255", true),     
-		new ResultPair("256.256.256.256", false),    
-		new ResultPair("255.com", true),             
-		new ResultPair("1.2.3.4.5", false),          
-		new ResultPair("1.2.3.4.", false),           
-		new ResultPair("1.2.3", false),              
-		new ResultPair(".1.2.3.4", false),           
-		new ResultPair("go.a", false),               
-		new ResultPair("go.a1a", false),             
-		new ResultPair("go.cc", true),               
-		new ResultPair("go.1aa", false),             
-		new ResultPair("aaa.", false),               
-		new ResultPair(".aaa", false),               
-		new ResultPair("aaa", false),                
-		new ResultPair("", false)      };
-
-	ResultPair[] testUrlAuthority = {new ResultPair("www.google.com", true),    
-		new ResultPair("go.com", true),              
-		new ResultPair("go.au", true),               
-		new ResultPair("0.0.0.0", true),             
-		new ResultPair("255.255.255.255", true),     
-		new ResultPair("256.256.256.256", false),    
-		new ResultPair("255.com", true),             
-		new ResultPair("1.2.3.4.5", false),          
-		new ResultPair("1.2.3.4.", false),           
-		new ResultPair("1.2.3", false),              
-		new ResultPair(".1.2.3.4", false),           
-		new ResultPair("go.a", false),               
-		new ResultPair("go.a1a", false),             
-		new ResultPair("go.cc", true),               
-		new ResultPair("go.1aa", false),             
-		new ResultPair("aaa.", false),               
-		new ResultPair(".aaa", false),               
-		new ResultPair("aaa", false),                
-		new ResultPair("", false)      };
-
-	ResultPair[] testPath = {new ResultPair("/test1", true),                    
-		new ResultPair("/t123", true),                       
-		new ResultPair("/$23", true),                        
-		new ResultPair("/..", false),                        
-		new ResultPair("/../", false),                       
-		new ResultPair("/test1/", true),                     
-		new ResultPair("", true),                            
-		new ResultPair("/test1/file", true),                 
-		new ResultPair("/..//file", false),                                 
-		new ResultPair("/test1//file", false)                
-	};  
-*/
